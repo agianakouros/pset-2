4caa6b83786102d2abfa8d176c0abdbd19439ed0
Alexander Gianakouros <agianakouros@ucvts.org>
2019-10-22 10:27:03 -0400

    problem3

---
+++ src/area.js
@@ -0,0 +1,28 @@
+const readlineSync = require("readline-sync");
+
+const width = readlineSync.question("\nWidth: ");
+const length = readlineSync.question("Length: ");
+
+
+const min = 25.4
+
+
+let a = width;
+let b = length;
+
+let c = a * min;
+let d = b * min;
+
+const area = (c * d).toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
+
+
+
+
+
+
+
+
+
+
+
+console.log("\nA(n) " + a + "-by-" + b + "-inch sheet of paper has an area of " + area + " square millimeter(s).");
