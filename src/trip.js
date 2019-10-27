97e5030c8ea1a19a6e46cc184452da6cbb259752
Alex G <agianakouros@ucvts.org>
2019-10-27 09:43:07 -0400

    almost done

---
+++ src/trip.js
@@ -0,0 +1,15 @@
+const readlineSync = require("readline-sync");
+
+
+
+const students = Number(readlineSync.question("\nStudents: "));
+const teachers = Number(readlineSync.question("Teachers: "));
+
+
+const bus = Number(readlineSync.question("Bus capacity: "));
+
+
+const total = (students + teachers)
+var busnumber = Math.floor((total/bus) + 1);   //I'm doing +1 because you need one extra bus for the people that make up the remainder. 
+
+console.log("\n" + busnumber + " bus(es) is (are) needed, with " + (total % bus) + " passenger(s) on the last bus.")
