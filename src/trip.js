4999755d86e2bc19f2de907154e6f211461bd815
Alex G <agianakouros@ucvts.org>
2019-10-27 14:49:27 -0400

    holy hell so many errors

--- src/trip.js
+++ src/trip.js
@@ -12,4 +12,4 @@ const bus = Number(readlineSync.question("Bus capacity: "));
 const total = (students + teachers)
 const busnumber = Math.floor((total/bus) + 1);   //I'm doing +1 because you need one extra bus for the people that make up the remainder.//
 
-console.log("\n" + busnumber + " bus(es) is (are) needed, with " + (total % bus) + " passenger(s) on the last bus.")
+console.log("\n" + busnumber + " bus(es) is (are) needed, with " + (total % bus) + " passenger(s) on the last bus.");
